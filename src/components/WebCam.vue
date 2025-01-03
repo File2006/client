c1d23f18ffb6ac69d0d0b65ae80edbe25365d086
file06 <file2006@seznam.cz>
2025-01-03 14:27:20 +0100

    Tak teď

---
+++ src/components/WebCam.vue
@@ -0,0 +1,17 @@
+<script setup>
+videoElement =  document.getElementById('videoElement');
+const stream = await navigator.mediaDevices.getUserMedia({'video': true, 'audio': true})
+  .then(stream => {
+    videoElement.srcObject = stream;
+    localStream = stream;
+  })
+.catch(err => console.error('Error accessing media devices:', err));
+</script>
+
+<template>
+<video id="videoElement" autoplay muted></video>
+</template>
+
+<style scoped>
+
+</style>
