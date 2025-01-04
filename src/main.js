d30cda783bd110539132a1cc97353c542bd4ca58
file06 <file2006@seznam.cz>
2025-01-04 15:24:28 +0100

    První test remote kamery

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue';  // Correct way to import Vue 3
 import App from './components/ExpressTest.vue';       // Import the root component
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://omeetly-c552ec79ca41.herokuapp.com';
+axios.defaults.baseURL = 'wss://omeetly-c552ec79ca41.herokuapp.com';
 const app = createApp(App);
 app.mount('#app');
