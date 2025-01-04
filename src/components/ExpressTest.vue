b6a4a793512c6ce3e847e7b802aa87884a35bd22
file06 <file2006@seznam.cz>
2025-01-04 17:35:21 +0100

    První test remote kamery

--- src/components/ExpressTest.vue
+++ src/components/ExpressTest.vue
@@ -1,11 +1,13 @@
 <script setup>
 import LocalWebCam from './LocalWebCam.vue';
+import RemoteWebCam from "@/components/RemoteWebCam.vue";
 </script>
 <template>
     <div class="top-bar">
       <img class ="icon" src="./icons/omeetly_text_final.png" alt="Omeetly_logo">
     </div>
      <local-web-cam></local-web-cam>
+  <remote-web-cam></remote-web-cam>
  </template>
  <style>
 @import "./../assets/main.css";
