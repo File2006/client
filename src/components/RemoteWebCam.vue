dadd5a23e4a2a263f5d72e01858051f16bfc8857
file06 <file2006@seznam.cz>
2025-01-04 18:10:34 +0100

    První test remote kamery

--- src/components/RemoteWebCam.vue
+++ src/components/RemoteWebCam.vue
@@ -9,9 +9,8 @@ signalingServer.onopen = () => {
 
 signalingServer.onmessage = (message) => {
 
-  const messageString = message instanceof Buffer ? message.toString() : message;
-  console.log('Received client message:', messageString);
-  const data = JSON.parse(messageString);
+  console.log('Received client message:', message);
+  const data = JSON.parse(message);
 
   if (data.offer) {
     peerConnection.setRemoteDescription(new RTCSessionDescription(data.offer))
