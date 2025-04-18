ff5d21774e830187eb9f3b65b9d29b0f60eddeb4
file06 <file2006@seznam.cz>
2025-04-18 12:16:08 +0200

    Vercel Working version

--- src/components/LocalWebCam.vue
+++ src/components/LocalWebCam.vue
@@ -5,7 +5,7 @@ const localVideo = ref(null);
 
 onMounted(async () => {
   try{
-    const localStream = await getLocalStream()
+    const localStream = getLocalStream()
     if (localVideo.value){
       localVideo.value.srcObject = localStream;
     }
